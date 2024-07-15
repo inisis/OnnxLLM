febed7c3b627381d49f93438640a5c314435a19e
inisis <desmond.yao@buaa.edu.cn>
2024-07-15 03:19:29 +0000

    add qwen,llama3,chatglm3

---
+++ onnxllm/models/auto_config.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+
+
+CONFIG_MAPPING_NAMES = OrderedDict(
+    [
+        ("qwen", "QWenConfig"),
+        ("llama3", "LlamaConfig"),
+        ("chatglm", "ChatGLMConfig"),
+    ]
+)
---
+++ onnxllm/models/auto_models.py
@@ -0,0 +1,19 @@
+from collections import OrderedDict
+from onnxllm.models.auto_config import CONFIG_MAPPING_NAMES
+from onnxllm.models.auto_factory import _BaseAutoModelClass, _LazyAutoMapping, auto_class_update
+
+
+MODEL_FOR_CAUSAL_LM_MAPPING_NAMES = OrderedDict(
+    [
+        ("qwen", "QwenForCausalLM"),
+        ("llama3", "LlamaForCausalLM"),
+        ("chatglm", "ChatGLMForCausalLM"),
+    ]
+)
+
+MODEL_FOR_CAUSAL_LM_MAPPING = _LazyAutoMapping(CONFIG_MAPPING_NAMES, MODEL_FOR_CAUSAL_LM_MAPPING_NAMES)
+
+class AutoModelForCausalLM(_BaseAutoModelClass):
+    _model_mapping = MODEL_FOR_CAUSAL_LM_MAPPING
+
+AutoModelForCausalLM = auto_class_update(AutoModelForCausalLM, head_doc="causal language modeling")
